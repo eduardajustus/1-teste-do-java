1b55e82abe38efae4785722b408ce26b644b9b67
eduardajustus <130786194+eduardajustus@users.noreply.github.com>
2023-08-04 16:41:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function primeiro(){
+    document.body.style.backgroundImage = "url('https://static.ifood-static.com.br/image/upload/t_high/logosgde/8c3038f7-d02b-495e-878e-94477df0be10/201907162301_E09M_i.png')"
+}
+function segundo(){
+    document.body.style.backgroundImage = "url('https://www.hypeness.com.br/1/2016/10/QizaiPanda_destaque.jpg')"
+}
+function terceiro(){
+    document.body.style.backgroundImage = "url('https://i.pinimg.com/236x/ff/33/12/ff3312667d48304428d4e8b28dd704bd.jpg')"
+}
